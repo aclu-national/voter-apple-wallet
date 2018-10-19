9813e43d3f20135cf3b8087a670993dfadf48e6f
Dan Phiffer <dan@phiffer.org>
2018-10-19 12:20:35 -0400

    :zap: use command line args for content

--- pkpass.js
+++ pkpass.js
@@ -4,72 +4,97 @@ const { Template } = require('@destinationstransfers/passkit');
 
 (async () => {
 
-	const template = await Template.load('./template', 'safeandfree');
-	var pass = template.createPass({
-		serialNumber: "1",
-		description: "ACLU Voter"
-	});
+	try {
 
-	var file = fs.readFileSync('./content.yaml', 'utf8');
-	const content = yaml.parse(file);
+		const template = await Template.load('./template', 'safeandfree');
+		var pass = template.createPass({
+			serialNumber: "1",
+			description: "ACLU Voter"
+		});
 
-	if (content.locations) {
-		console.log('Adding locations:');
-		console.log(content.locations);
-		pass.fields.locations = content.locations;
-	}
-
-	var num = 0;
-	if (content.primary) {
-		for (let item of content.primary) {
-			num++;
-			console.log(`primary: ${item}`);
-			pass.primaryFields.add('primary' + num, 'this is ignored', item);
+		if (process.argv.length < 6) {
+			console.log('Usage: node pkpass.js [address] [hours] [lat] [lng]');
+			process.exit(1);
 		}
-	}
 
-	num = 0;
-	if (content.secondary) {
-		for (let item of content.secondary) {
-			num++;
-			for (let key in item) {
-				console.log(`secondary ${key}: ${item[key]}`);
-				pass.secondaryFields.add('secondary' + num, key, item[key]);
+		const address = process.argv[2];
+		const hours = process.argv[3];
+		const lat = process.argv[4];
+		const lng = process.argv[5];
+
+		const content = {
+			secondary: [
+				{ 'Polling Place Address': address }
+			],
+			auxiliary: [
+				{ 'Hours': hours },
+				{ 'Learn More': 'aclu.org/voter' }
+			],
+			backFields: [
+				{ 'Polling Place Address': address },
+				{ 'Polling Place Hours': hours }
+			]
+		};
+
+		if (content.locations) {
+			console.log('Adding locations:');
+			console.log(content.locations);
+			pass.fields.locations = content.locations;
+		}
+
+		var num = 0;
+		if (content.primary) {
+			for (let item of content.primary) {
+				num++;
+				console.log(`primary: ${item}`);
+				pass.primaryFields.add('primary' + num, 'this is ignored', item);
 			}
 		}
-	}
 
-	num = 0;
-	if (content.auxiliary) {
-		for (let item of content.auxiliary) {
-			num++;
-			for (let key in item) {
-				console.log(`auxiliary ${key}: ${item[key]}`);
-				pass.auxiliaryFields.add('auxiliary' + num, key, item[key]);
+		num = 0;
+		if (content.secondary) {
+			for (let item of content.secondary) {
+				num++;
+				for (let key in item) {
+					console.log(`secondary ${key}: ${item[key]}`);
+					pass.secondaryFields.add('secondary' + num, key, item[key]);
+				}
 			}
 		}
-	}
 
-	num = 0;
-	if (content.backFields) {
-		for (let item of content.backFields) {
-			num++;
-			for (let key in item) {
-				console.log(`backFields ${key}: ${item[key]}`);
-				pass.backFields.add('backFields' + num, key, item[key]);
+		num = 0;
+		if (content.auxiliary) {
+			for (let item of content.auxiliary) {
+				num++;
+				for (let key in item) {
+					console.log(`auxiliary ${key}: ${item[key]}`);
+					pass.auxiliaryFields.add('auxiliary' + num, key, item[key]);
+				}
 			}
 		}
+
+		num = 0;
+		if (content.backFields) {
+			for (let item of content.backFields) {
+				num++;
+				for (let key in item) {
+					console.log(`backFields ${key}: ${item[key]}`);
+					pass.backFields.add('backFields' + num, key, item[key]);
+				}
+			}
+		}
+
+		pass.fields.relevantDate = "2018-11-06T10:00-04:00";
+
+		let file = fs.createWriteStream("pass.pkpass");
+		pass.on("error", function(error) {
+			console.error(error);
+			process.exit(1);
+		})
+		pass.pipe(file);
+
+	} catch(err) {
+		console.log(err.stack);
 	}
-
-	pass.fields.relevantDate = "2018-09-26T10:00-04:00";
-
-	console.log(pass.fields);
-
-	file = fs.createWriteStream("pass.pkpass");
-	pass.on("error", function(error) {
-		console.error(error);
-		process.exit(1);
-	})
-	pass.pipe(file);
 
 })();
