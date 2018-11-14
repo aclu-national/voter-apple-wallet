e1104640fa0201279998fd7fa9dbc4aae776ed5f
Dan Phiffer <dan@phiffer.org>
2018-11-13 19:15:09 -0500

    :key: use MAPBOX_API_KEY environment var

--- pkpass.sh
+++ pkpass.sh
@@ -4,7 +4,6 @@ node=`which node`
 composite=`which composite`
 curl=`which curl`
 base_dir="/usr/local/aclu/voter-apple-wallet"
-mapbox_api_key="pk.eyJ1IjoiYWNsdSIsImEiOiJjamtzcG13eXIwMnA3M3dxbzhvZnlqaGNxIn0.dt0nTfFyf__f25JbQQtoaw"
 mapbox_api_url="https://api.mapbox.com/styles/v1/mapbox/light-v9/static"
 zoom=15
 
@@ -29,8 +28,8 @@ if [ ! -f "$base_dir/passes/$hash/pass.pkpass" ] ; then
 	dir="$base_dir/passes/$hash"
 	cp -R "$base_dir/template" "$dir"
 
-	$curl -s "$mapbox_api_url/$lng,$lat,$zoom.0,0,0/375x144?access_token=$mapbox_api_key" > "$dir/map.png"
-	$curl -s "$mapbox_api_url/$lng,$lat,$zoom.0,0,0/375x144@2x?access_token=$mapbox_api_key" > "$dir/map@2x.png"
+	$curl -s "$mapbox_api_url/$lng,$lat,$zoom.0,0,0/375x144?access_token=$MAPBOX_API_KEY" > "$dir/map.png"
+	$curl -s "$mapbox_api_url/$lng,$lat,$zoom.0,0,0/375x144@2x?access_token=$MAPBOX_API_KEY" > "$dir/map@2x.png"
 	$composite -gravity center "$pin" "$dir/map.png" "$dir/strip.png"
 	$composite -gravity center "$pin2x" "$dir/map@2x.png" "$dir/strip@2x.png"
 
